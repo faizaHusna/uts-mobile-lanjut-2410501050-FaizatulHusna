05e1a23e362a1a9cfa4c317fd6cce9b244195c9b
faizaHusna <faizatuulhusna@gmail.com>
2026-04-28 21:02:35 +0700

    Menambahkan loading indicator dan error handing serta testing pada HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,14 +6,26 @@ import { fetchMeals } from '../services/mealApi';
 
 const HomeScreen = ({ navigation }) => {
     const [meals, setMeals] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
 
     useEffect(() => {
         loadMeals();
     }, []);
 
     const loadMeals = async () => {
+        try {
+            setLoading(true);
+            setError(null);
+        
         const data = await fetchMeals();
-       
+        setMeals(data || []);
+        } catch (err) {
+            setError('Gagal mengambil data');
+        } finally {
+            setLoading(false);
+        }   
         
     };
 
@@ -21,13 +33,21 @@ const HomeScreen = ({ navigation }) => {
         navigation.navigate('Detail', { meal });
     };
 
-    
+    if (loading) {
+        return <Text>Loading...</Text>;
+    }
+
+    if (error) {
+        return <Text>{error}</Text>;
+    }
+ 
 
     return (
         <View>
-            <FlatList data={meals}
+            <FlatList 
+            data={meals}
                 keyExtractor={(item) => item.idMeal}
-                renderItem={({ item}) => (
+                renderItem={({ item }) => (
                     <TouchableOpacity onPress={() => handleMealPress(item)}>
                         <Text>{item.strMeal}</Text>
                     </TouchableOpacity>
--- src/services/mealApi.js
+++ src/services/mealApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://www.themealdb.com/api/json/v1/1';
+const BASE_URL = 'https://www.google.com/search?q=facebok&oq=facebok&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIXCAEQLhgKGAsYgwEYxwEYsQMY0QMYgAQyEQgCEAAYChgLGIMBGLEDGIAEMhEIAxAAGAoYCxiDARixAxiABDIRCAQQABgKGAsYgwEYsQMYgAQyDggFEAAYChgLGLEDGIAEMg4IBhAAGAoYCxixAxiABDILCAcQABgKGAsYgAQyCwgIEAAYChgLGIAE0gEIMjc5NWowajeoAgCwAgA&sourceid=chrome&ie=UTF-8';
 
 export const fetchMeals = async (query = 'chicken') => {
     const response = await fetch(`${BASE_URL}/search.php?s={query}`);
