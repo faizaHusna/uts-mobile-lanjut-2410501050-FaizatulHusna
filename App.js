83543df230835ac7b1cf3f5299909a1f3388402b
faizaHusna <faizatuulhusna@gmail.com>
2026-04-29 19:22:59 +0700

    Menambahkan Context API dan useReducer untuk fitur favorit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { FavoritesProvider } from './src/context/FavoritesContext';
 import StackNavigator from './src/navigation/StackNavigator';
 export default function App() {
     return (
         
+            <FavoritesProvider>
             <NavigationContainer>
+                <TabNavigator />
                 <StackNavigator />
                 </NavigationContainer>
+                </FavoritesProvider>
         
     );
     
---
+++ src/context/FavoritesContext.js
@@ -0,0 +1,84 @@
+import { createContext, useContext, useReducer } from 'react';
+
+const ADD_FAVORITE = 'ADD-FAVORITE';
+const REMOVE_FAVOORITE = 'REMOVE_FAVORITE';
+
+const initialState = {
+    favorites: [],
+
+};
+
+const favoritesReducer = (state, action) => {
+    switch (action.type) {
+        case ADD_FAVORITE: {
+
+            const alreadyExists = state.favorites.some(
+                (meal) => meal.idMeal === action.playload.idMeal
+            );
+            if (alreadyExists) {
+                return state;
+            } 
+            return {
+                ...state,
+                favorites: [...state.favorites, action.playload],
+            };
+        }
+
+        case REMOVE_FAVOORITE: {
+            return {
+                ...state,
+                favorites: state.favorites.filter(
+                    (meal) => meal.idMeal !== action.playload
+                ),
+            };
+        
+        }
+
+        default:
+        return state;
+
+    }
+
+    };
+
+
+
+export const FavoritesContext = createContext();
+
+
+export const FavoritesProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(favoritesReducer, initialState);
+    
+    const addFavorite = (meal) => {
+        dispatch({ type: ADD_FAVORITE, playload: meal });
+    };
+
+    const removeFavorite = (mealId) => {
+        dispatch({ type: REMOVE_FAVOORITE, playload: mealId });
+    };
+
+    const isFavorite = (mealId) => {
+        return state.favorites.some((meal) => meal.idMeal === mealId);
+    };
+
+    const value = {
+        favorites: state.favorites,
+        addFavorite,
+        removeFavorite,
+        isFavorite,
+    };
+
+    return (
+        <FavoritesContext.Provider value={value}>
+            {children}
+        </FavoritesContext.Provider>
+    );
+    };
+
+    export const useFavorites = () => {
+        const context = useContext(FavoritesContext);
+        if (!context) {
+            throw new Error('useFavorites harus digunakan di dalam FavoritesProvider');
+        }
+        return context;
+    };
